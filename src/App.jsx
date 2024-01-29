83cd54db03b68da7f4401a3e2a950d5fe1a42a4f
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-01-29 12:53:27 +0000

    fetching data and loading/error state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,55 @@
+/* eslint react/prop-types: 0 */
+
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <h1>Hello test</h1>;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  async function fetchCountries() {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+
+      const res = await fetch(`https://restcountries.com/v3.1/all`);
+      const data = await res.json();
+      setData(data);
+    } catch (err) {
+      setIsError(true);
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(function () {
+    fetchCountries();
+  }, []);
+
+  return (
+    <div className="wrapper">
+      {isLoading && <Loader />}
+      {isError && <Error />}
+      {!isLoading && !isError && <Country data={data} />}
+    </div>
+  );
+}
+
+function Country({ data }) {
+  return (
+    <div className="country__box">
+      {data.map((country) => (
+        <p key={country.name.common}>{country.name.common}</p>
+      ))}
+    </div>
+  );
+}
+
+function Loader() {
+  return <p className="loader">Loading countries...</p>;
+}
+
+function Error() {
+  return <p className="error">There was an error fetching countries...</p>;
 }
